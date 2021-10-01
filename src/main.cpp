f2050519f6f3283b579b26a2eacd74dcdca990ad
Zoilo Mora <zoilo.mora@hotmail.com>
2021-10-01 20:04:05 +0200

    Add other libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+#include <Ticker.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <TasmotaModbus.h>
 
 void setup() {
   // put your setup code here, to run once:
